cf06bda5655b745621663e7690789888f5eeb596
itsernestt <94154145+itsernestt@users.noreply.github.com>
2022-04-24 18:10:42 +0800

    Update Question5.java

--- Question5.java
+++ Question5.java
@@ -1,32 +1,45 @@
 import java.util.Scanner;
+import java.util.*;
 
+//NOT WORKING
 public class Question5
 {
   public static void main(String[] args)
   {
-    /**
-     * Prompt the user for number. This input indicates the number of integers the user will be entering next. 
-     * Print out the mode (highest occurrence) from the set of integers. 
-     *    e.g.
-     *     > 5
-     *     > 2
-     *     > 4
-     *     > 1
-     *     > 3
-     *     > 4
-     *     4
-     * 
-     *    e.g.
-     *     > 4
-     *     > 2
-     *     > 2
-     *     > 3
-     *     > 3
-     *     2
-     * Hint: Use a loop to get input. Use another 2 loops to find the mode
-     */
-     
+  
     Scanner in = new Scanner(System.in);
-    
+    int firstnum = in.nextInt();
+    List<Integer> list = new ArrayList<Integer>();
+    List<Integer> countlist = new ArrayList<Integer>();
+    int count = 0;
+    for (int i = firstnum - 1; i >= 0; i--)
+      {
+        int secondnum = in.nextInt();
+        list.add(secondnum);
+      }
+    Collections.sort(list);
+    System.out.print("Your list is " + list);
+    for (int x = 0; x >= list.size(); x++)
+      {
+        if (x > 0)
+        {
+          if (list.get(x) == list.get(x-1))
+          {
+            count = count + 1;
+            countlist.add(count);
+          }
+          else
+          {
+            count = 0;
+          }
+        }
+        
+        else
+        {
+          count = 0;
+        }
+        
+    }
+    System.out.print("\nCountlist: " + countlist);
   }
 }
